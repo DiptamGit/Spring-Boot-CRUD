dcb7199c7fed215d7e8444c6d4a03a93421ab1ee
diptamsarkar@gmail.com <dipgit17>
2019-12-21 11:07:23 -0600

    <<Added @Id>>

--- src/main/java/com/diptam/SpringBootCRUD/model/Actor.java
+++ src/main/java/com/diptam/SpringBootCRUD/model/Actor.java
@@ -3,12 +3,14 @@ package com.diptam.SpringBootCRUD.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 public class Actor {
 
+    @Id
     private Long actor_id;
 
     private String first_name;
